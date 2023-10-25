7d5f75bfcaa34a3b36e9407347f6db0be198765a
Jakub Nitsch <jakubnitsch@gmail.com>
2023-10-25 13:12:10 +0200

    refactor: php-cs-fixer

--- src/Breadcrumb.php
+++ src/Breadcrumb.php
@@ -77,7 +77,7 @@ class Breadcrumb
      */
     public function render(string $class = '', string $listItems = '', bool $includeHome = false): string
     {
-        if ($includeHome){
+        if ($includeHome) {
             $homeLink['text']   = lang('Menu.home');
             $homeLink['href']   = base_url();
             $homeLink['active'] = current_url() === base_url();
